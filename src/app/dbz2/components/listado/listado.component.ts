0b3bf9bc35fb150145e7abdda5b3f46f2db2b1e4
Yumar <daylocyumar@gmail.com>
2024-12-06 11:19:25 +0100

    Ya estamos en DBZ

---
+++ src/app/dbz2/components/listado/listado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz2-listado',
+  standalone: false,
+
+  templateUrl: './listado.component.html',
+  styleUrl: './listado.component.css'
+})
+export class ListadoComponent {
+
+}
--- src/app/dbz2/dbz2.module.ts
+++ src/app/dbz2/dbz2.module.ts
@@ -2,13 +2,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DbzComponent } from './pages/main-page.component';
+import { ListadoComponent } from './components/listado/listado.component';
+import { AgregarComponent } from './components/agregar/agregar.component';
 
 
 
 
 
 @NgModule({
-  declarations: [DbzComponent],
+  declarations: [DbzComponent, ListadoComponent, AgregarComponent],
   exports:[DbzComponent],
   imports: [
     CommonModule
